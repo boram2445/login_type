4c858ec624c0f47546d66333320f4e01d6224ffa
Boram Kim <boram2445@gmail.com>
2022-05-16 10:09:43 +0900

    [0516] 드랍다운 아이템 선택시 닫히도록 수정

--- drop_down/main.js
+++ drop_down/main.js
@@ -1,6 +1,5 @@
 "use-strict";
 
-//1. btn-select를 선택하면 cont-list가 나타나게 한다.
 const selectBtn = document.querySelector('.btn-select');
 const lists = document.querySelector('.cont-lists');
 
@@ -8,10 +7,10 @@ selectBtn.addEventListener('click', ()=>{
   selectBtn.classList.toggle('on');
 })
 
-//2. cont-lists를 선택하면 btn-select의 id값을 받아와 textContent를 변경한다.  
 lists.addEventListener('click',(event)=>{
   if(event.target.tagName !== 'BUTTON'){
     return;
   }
   selectBtn.textContent = event.target.textContent;
+  selectBtn.classList.remove('on');
 })
